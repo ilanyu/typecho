7e82a5d7010d14f03e1a2a3e3e59337137b7583c
ilanyu <lanyu19950316@gmail.com>
2015-11-24 18:24:42 +0800

    init

---
+++ src/main/webapp/WEB-INF/pages/sidebar.jsp
@@ -0,0 +1,27 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<div class="am-u-md-4 blog-sidebar">
+    <div class="am-panel-group">
+        <section class="am-panel am-panel-default">
+            <div class="am-panel-hd">捐赠</div>
+            <div class="am-panel-bd">
+                <div class="am-u-sm-centered"><img src="http://blog.lanyus.com/alipay.png" /></div>
+            </div>
+        </section>
+        <section class="am-panel am-panel-default">
+            <div class="am-panel-hd">最新文章</div>
+            <ul class="am-list blog-list" id="newArticle">
+            </ul>
+        </section>
+
+        <section class="am-panel am-panel-default">
+            <div class="am-panel-hd">其他</div>
+            <ul class="am-list blog-list">
+                <li><a href="/admin">后台管理</a></li>
+                <li><a href="/feed/">文章 RSS</a></li>
+                <li><a href="/feed/comments/">评论 RSS</a></li>
+                <li><a href="http://typecho.org/">Typecho</a></li>
+                <li><a href="http://blog.lanyus.com/">ilanyu</a></li>
+            </ul>
+        </section>
+    </div>
+</div>
---
+++ src/main/webapp/js/function.js
@@ -0,0 +1,57 @@
+function getUrlParam(name){
+    //构造一个含有目标参数的正则表达式对象
+    var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+    //匹配目标参数
+    var r = window.location.search.substr(1).match(reg);
+    //返回参数值
+    if (r!=null) return unescape(r[2]);
+    return null;
+}
+function getPageList() {
+    $.getJSON("./getPageList","", function (data) {
+        var count = data.length;
+        for (var i = 0; i < count ; i++) {
+            if (data[i]["cid"] == getUrlParam("cid")) {
+                $('#nav').append("<li class='am-active'><a href='/page?cid=" + data[i]["cid"] + "'>" + data[i]["title"] + "</a></li>");
+                $('#index').removeClass("am-active");
+            } else {
+                $('#nav').append("<li><a href='/page?cid=" + data[i]["cid"] + "'>" + data[i]["title"] + "</a></li>");
+            }
+        }
+    });
+}
+function getIndexBlog(start,limit) {
+    var count = 0;
+    $.ajaxSettings.async = false;
+    $.getJSON("./getIndexBlog",{"start":start,"limit":limit}, function (data) {
+        count = data.length;
+        for (var i = 0; i < count ; i++) {
+            $('#article').append('<article class="blog-main"><h3 class="am-article-title blog-title"><a href="/blog?cid=' + data[i]["cid"] + '">' + data[i]["title"] + '</a></h3><h4 class="am-article-meta blog-meta">by <a href="/author?uid=' + data[i]["authorId"] + '">' + data[i]["author"] + '</a> posted on ' + data[i]["date"] + ' under <a href="' + data[i]["categorySlug"] + '">' + data[i]["category"] + '</a></h4><div class="am-g blog-content">' + data[i]["content"] + '</div></article>');
+            $('#article').append('<hr class="am-article-divider blog-hr">');
+        }
+    });
+    $.ajaxSettings.async = true;
+    return count;
+}
+function getNewBlogList() {
+    var count = 0;
+    $.getJSON("./getNewBlogList",{"limit":"10"}, function (data) {
+        count = data.length;
+        for (var i = 0 ; i < count ; i++) {
+            $('#newArticle').append('<li><a href="/blog?cid=' + data[i]["cid"] + '">' + data[i]["title"] + '</a></li>');
+        }
+    });
+    return count;
+}
+function getBlog() {
+    var cid = getUrlParam("cid");
+    $.getJSON("/getBlog/" + cid , "", function (data) {
+        $('#article').append('<article class="blog-main"><h3 class="am-article-title blog-title">' + data["title"] + '</h3><h4 class="am-article-meta blog-meta">by <a href="/author?uid=' + data["authorId"] + '">' + data["author"] + '</a> posted on ' + data["date"] + ' under <a href="' + data["categorySlug"] + '">' + data["category"] + '</a></h4><div class="am-g blog-content">' + data["content"] + '</div></article>');
+    });
+}
+function getPage() {
+    var cid = getUrlParam("cid");
+    $.getJSON("/getPage/" + cid , "", function (data) {
+        $('#article').append('<article class="blog-main"><h3 class="am-article-title blog-title">' + data["title"] + '</h3><div class="am-g blog-content">' + data["content"] + '</div></article>');
+    });
+}
